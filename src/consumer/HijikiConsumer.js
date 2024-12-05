9be1dccfb12f14a091faa8dea3cde7852786e11d
battisti <asengardeons@hotmail.com>
2024-12-05 17:41:06 -0300

    fix(Consumer): ajustado para consumir mensagens com erro e enviart para DLQ na falha

--- src/consumer/HijikiConsumer.js
+++ src/consumer/HijikiConsumer.js
@@ -59,29 +59,29 @@ class Consumer{
         return result.concat(result_dlq)
     }
 
-
-    task(queue_name, callback){
-        let queues = this.create_queues().filter(item => item.queue === queue_name)
-
-        for (const q of queues) {
-            let sub = this.broker.connection.createConsumer(q, (msg) => {
-                try{
-                    this.process_message(msg.body, callback);
-                }catch (err){
-                    if(!this.broker.auto_ack)
-                        return ConsumerStatus.REQUEUE
-                }
-            })
-
-            sub.on('error', (err) => {
-                // Maybe the consumer was cancelled, or the connection was reset before a
-                // message could be acknowledged.
-                console.log(`Consumer error for ${queue_name}`, err)
-                if(!this.broker.auto_ack)
+    crete_consumer(queue, callback, is_dlq){
+        let sub = this.broker.connection.createConsumer(queue, (msg) => {
+            try{
+                this.process_message(msg.body, callback);
+                return ConsumerStatus.ACK
+            }catch (err){
+                if(!this.broker.auto_ack && !is_dlq)
                     return ConsumerStatus.REQUEUE
-            })
-            this.subs.push(sub)
-        }
+            }
+        })
+
+        sub.on('error', (err) => {
+            // Maybe the consumer was cancelled, or the connection was reset before a
+            // message could be acknowledged.
+            console.log(`Consumer error for ${queue_name}`, err)
+            if(!this.broker.auto_ack && !is_dlq)
+                return ConsumerStatus.REQUEUE
+        })
+        this.subs.push(sub)
+    }
+    task(queue_name, callback, is_dlq){
+        let q = this.create_queues().find(item => item.queue === queue_name)
+        this.crete_consumer(q, callback, is_dlq)
         return this
     }
 }
---
+++ tests/integrated/hijiki_rabbit_dlq.test.js
@@ -0,0 +1,19 @@
+import {BrokerMock, delay} from "./base";
+
+let mock
+
+beforeEach(async () => {
+    mock = await new BrokerMock().init()
+});
+
+afterEach(() => {
+    mock.broker.terminate()
+})
+
+
+xtest('internal_consumer_erro', async () => {
+    mock.broker.publish_message('erro_event', `{"value": "This is the error message"} ${Date.now()}`)
+    await delay(3000)
+    expect(mock.result_event_list.length).toBe(11)
+    await delay(3000)
+}, 10000)
